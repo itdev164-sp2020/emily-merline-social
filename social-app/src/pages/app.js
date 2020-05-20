9205d101b8dbd9f19a9293c1fc369a364513047e
merlinee <merline1@gmatc.matc.edu>
2020-05-19 19:34:33 -0500

    Fixed typo

--- social-app/src/pages/app.js
+++ social-app/src/pages/app.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Router } from "@reach/router"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import PrivateRoute from "../components/private-route"
 // import Profile from "../components/Profile"
 // import Details from "../components/Details"
