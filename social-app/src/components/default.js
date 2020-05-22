12f09c8fbab746e46005a13822392f3f549cb01c
merlinee <merline1@gmatc.matc.edu>
2020-05-21 20:01:24 -0500

    Finished activity 9

--- social-app/src/components/default.js
+++ social-app/src/components/default.js
@@ -1,5 +1,5 @@
-/* import React from "react"
-import { useQuery } from '@apollo/react-hooks'
+import React, { useState } from "react"
+import { useQuery, useMutation } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 
 const GET_POSTS = gql `
@@ -17,6 +17,19 @@ query getPosts {
   }
 `  
 
+const SET_POST = gql `
+mutation createPost($title: String!, $content: String!) {
+    insert_posts(
+        objects: {
+            content: $content, 
+            title: $title
+        }
+        ) {
+        affected_rows
+    }
+  }
+`  
+
 const Post = ({post}) => {
     return (
         <div>
@@ -25,6 +38,47 @@ const Post = ({post}) => {
             </h3>
         <div>{post.content}</div>
         </div>
+    )
+}
+
+const PostEditor = () => {
+    const [post, setPost] = useState({title: '', content: ''})
+    const [createPost, {data}] = useMutation(SET_POST)
+    const handleChange = e => {
+        setPost({
+            ...post,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        createPost( { variables: { title: post.title, content: post.content }})
+        setPost({ title: '', content: ''})
+        
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <p><label htmlFor="title">Title</label>
+            <input 
+            type="text"
+            onChange={handleChange} 
+            name="title" 
+            id="title" 
+            value={post.title}
+            placeholder="Post Title"/>
+            </p><p>
+            <label htmlFor="content">Content</label>
+            <input 
+            type="text"
+            onChange={handleChange}  
+            name="content" 
+            id="content" 
+            value={post.content}
+            placeholder="Write some stuff!"/>
+            </p>
+            <button type="submit">Create</button>
+        </form>
     )
 }
 export default () => {
@@ -37,8 +91,8 @@ export default () => {
         return <pre>{JSON.stringify(error)}</pre>
     return (
         <div>
-            {data.posts.map(p =>(<Post post={p}/>))}
+            <PostEditor />
+            {data.posts.map(p =>(<Post post={p} key={p.id}/>))}
         </div>
     )
 } 
- */
--- social-app/src/pages/app.js
+++ social-app/src/pages/app.js
@@ -3,9 +3,9 @@ import { Router } from "@reach/router"
 import Layout from "../components/layout"
 import PrivateRoute from "../components/private-route"
 import Login from "../components/Login"
-//import Default from "../components/default"
+import Default from "../components/default"
 
-const Default = () => <h1>Default</h1>
+//const Default = () => <h1>Default</h1>
 const Profile = () => <h1>Profile</h1>
 const Details = () => <h1>Details</h1>
 const App = () => {
